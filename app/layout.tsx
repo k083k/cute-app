7c9a107775a43c442b5a6c0a10df9fdf122a60ce
Kwabena Opoku Frempong-Kore <kobekore@gmail.com>
2025-12-02 22:52:45 +0000

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,12 +30,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-50 min-h-screen flex flex-col`}
       >
+        <Analytics />
         <Navigation />
         <main className="flex-1 max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8 py-8">
           {children}
         </main>
         <Footer />
-        <Analytics />
       </body>
     </html>
   );
