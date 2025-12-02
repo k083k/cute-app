b0b0bcb1215a6d403f462c1a5e92ae88f81c8ed3
Kwabena Opoku Frempong-Kore <kobekore@gmail.com>
2025-12-02 23:41:37 +0000

    analytics fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,10 +30,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-50 min-h-screen flex flex-col`}
       >
-        <Analytics />
         <Navigation />
         <main className="flex-1 max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8 py-8">
           {children}
+          <Analytics />
         </main>
         <Footer />
       </body>
